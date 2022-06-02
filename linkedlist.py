8f716b7c27f6b9bc26b81189992faa31572a1589
ZandoCommando <xboyzander@gmail.com>
2022-06-02 09:21:06 -0700

    Sixth Commit: Dunder repr Created

--- linkedlist.py
+++ linkedlist.py
@@ -4,3 +4,13 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def __repr__(self):
+        returnStr = ""
+        current = self.head
+        while current is not None:
+            returnStr += str(current.get_value()) + "\n"
+            current = current.get_next_node()
+
+        return returnStr
+
+    
