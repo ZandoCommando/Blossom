1122feca06ce6d11841e167e7ae6c1e7dca15f07
ZandoCommando <xboyzander@gmail.com>
2022-06-02 09:17:45 -0700

    Fourth Commit: Setters and Getters Defined

--- node.py
+++ node.py
@@ -3,4 +3,11 @@ class Node:
         self.value = value
         self.next_node = next_node
 
-    
+    def get_value(self):
+        return self.value
+
+    def get_next_node(self):
+        return self.next_node
+
+    def set_next_node(self, next_node):
+        self.next_node = next_node
